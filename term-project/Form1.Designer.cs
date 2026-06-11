89876ac394aef4221845559f72d48dc626776481
威立 <66502303+willy1236@users.noreply.github.com>
2026-06-11 20:57:54 +0800

    feat: 新增用戶註冊與登入功能
    - 新增 Form1.Designer.cs 和 Form1.cs，實現用戶註冊和登入功能。
    - 使用 SQLite 作為資料庫，並加密用戶密碼。
    - 整合 Google Authenticator 進行二次驗證，生成 QR Code。
    - 新增必要的資源文件和設置文件以支持應用程序運行。

---
+++ term-project/Form1.Designer.cs
@@ -0,0 +1,249 @@
+﻿namespace term_project
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button2 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.picQr = new System.Windows.Forms.PictureBox();
+            this.txtCode = new System.Windows.Forms.TextBox();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.txtSecret = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picQr)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(149, 139);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 31);
+            this.button2.TabIndex = 13;
+            this.button2.Text = "登入";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(32, 205);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(301, 216);
+            this.dataGridView1.TabIndex = 12;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(42, 139);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 31);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "註冊";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(81, 74);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(156, 22);
+            this.textBox2.TabIndex = 10;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(81, 35);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(156, 22);
+            this.textBox1.TabIndex = 9;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(30, 74);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 12);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "密碼";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(30, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 12);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "帳號";
+            // 
+            // picQr
+            // 
+            this.picQr.Location = new System.Drawing.Point(652, 51);
+            this.picQr.Name = "picQr";
+            this.picQr.Size = new System.Drawing.Size(250, 250);
+            this.picQr.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picQr.TabIndex = 21;
+            this.picQr.TabStop = false;
+            // 
+            // txtCode
+            // 
+            this.txtCode.Location = new System.Drawing.Point(444, 339);
+            this.txtCode.Name = "txtCode";
+            this.txtCode.Size = new System.Drawing.Size(100, 22);
+            this.txtCode.TabIndex = 20;
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Location = new System.Drawing.Point(650, 318);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(33, 12);
+            this.lblResult.TabIndex = 19;
+            this.lblResult.Text = "label3";
+            // 
+            // txtSecret
+            // 
+            this.txtSecret.Location = new System.Drawing.Point(444, 136);
+            this.txtSecret.Name = "txtSecret";
+            this.txtSecret.Size = new System.Drawing.Size(151, 22);
+            this.txtSecret.TabIndex = 18;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(356, 342);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(65, 12);
+            this.label3.TabIndex = 17;
+            this.label3.Text = "輸入驗證碼";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(714, 342);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(147, 57);
+            this.button3.TabIndex = 16;
+            this.button3.Text = "驗證";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(372, 139);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(49, 12);
+            this.label4.TabIndex = 15;
+            this.label4.Text = "SecreKey";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(385, 67);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(87, 50);
+            this.button4.TabIndex = 14;
+            this.button4.Text = "綁定Google Authenticator";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(30, 105);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(29, 12);
+            this.label5.TabIndex = 22;
+            this.label5.Text = "名稱";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(81, 105);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(156, 22);
+            this.textBox3.TabIndex = 23;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(938, 450);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.picQr);
+            this.Controls.Add(this.txtCode);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.txtSecret);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picQr)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox picQr;
+        private System.Windows.Forms.TextBox txtCode;
+        private System.Windows.Forms.Label lblResult;
+        private System.Windows.Forms.TextBox txtSecret;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox3;
+    }
+}
+
---
+++ term-project/Form1.cs
@@ -0,0 +1,217 @@
+﻿using OtpNet;
+using QRCoder;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SQLite;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Shapes;
+
+namespace term_project
+{
+    public partial class Form1 : Form
+    {
+        string pub_xml, pvt_xml;
+        RSAParameters pub_parameter, pvt_parameter;
+        string key_dir = @".\key";
+
+        string db_path = "User.db";
+        string connection_str;
+        SQLiteConnection conn;
+        string sql;
+        SQLiteCommand cmd;
+
+        private const string Issuer = "GaOTPTest";
+        public Form1()
+        {
+            InitializeComponent();
+            connection_str = $"Data Source={db_path};" + "Version=3;";
+            if (!File.Exists(db_path))
+            {
+                SQLiteConnection.CreateFile(db_path);
+                using (conn = new SQLiteConnection(connection_str))
+                {
+                    conn.Open();
+
+                    sql = @"CREATE TABLE IF NOT EXISTS Users(
+                            Id TEXT Primary Key,
+                            Name TEXT NOT NULL,
+                            Password TEXT NOT NULL,
+                            SecreKey TEXT NULL);
+                            ";
+                    cmd = new SQLiteCommand(sql, conn);
+                    cmd.ExecuteNonQuery();
+                }
+            }
+
+            Load_key();
+        }
+
+        public void Load_key()
+        {
+            if (File.Exists(key_dir + @"\pvt.xml"))
+            {
+                StreamReader sr = new StreamReader(key_dir + @"\pub.xml");
+                pub_xml = "";
+                while(!sr.EndOfStream)
+                {
+                    pub_xml += sr.ReadLine();
+                }
+                sr.Close();
+
+                sr = new StreamReader(key_dir + @"\pvt.xml");
+                pvt_xml = "";
+                while (!sr.EndOfStream)
+                {
+                    pvt_xml += sr.ReadLine();
+                }
+                sr.Close();
+            }
+            else
+            {
+                Create_key();
+            }
+        }
+        public static string EncryptRSA(string original, string xmlString)
+        {
+            try
+            {
+                RSACryptoServiceProvider rsa = new RSACryptoServiceProvider();
+                rsa.FromXmlString(xmlString);
+                byte[] s = Encoding.UTF8.GetBytes(original);
+                return BitConverter.ToString(rsa.Encrypt(s, false)).Replace("-", string.Empty);
+            }
+            catch
+            {
+                return original;
+            }
+        }
+        public static string DecryptRSA(string hexstring, string xmlString)
+        {
+            try
+            {
+                RSACryptoServiceProvider rsa = new RSACryptoServiceProvider();
+                rsa.FromXmlString(xmlString);
+                byte[] s = new byte[hexstring.Length / 2];
+                for (int i = 0; i < hexstring.Length; i += 2)
+                {
+                    s[i / 2] = Byte.Parse(hexstring[i].ToString() + hexstring[i + 1].ToString(),
+                        System.Globalization.NumberStyles.HexNumber);
+                }
+                return Encoding.UTF8.GetString(rsa.Decrypt(s, false));
+            }
+            catch
+            {
+                return hexstring;
+            }
+        }
+
+
+        public string Generate_secret()
+        {
+            return Base32Encoding.ToString(KeyGeneration.GenerateRandomKey(20));
+        }
+        public void Create_key()
+        {
+            RSACryptoServiceProvider rsa = new RSACryptoServiceProvider();
+            pub_xml = rsa.ToXmlString(false);
+            pvt_xml = rsa.ToXmlString(true);
+            pub_parameter = rsa.ExportParameters(false);
+            pvt_parameter = rsa.ExportParameters(true);
+
+            try
+            {
+                DirectoryInfo di = Directory.CreateDirectory(key_dir);
+                MessageBox.Show("密鑰子目錄建立成功，時間：" + Directory.GetCreationTime(key_dir).ToString());
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("密鑰子目錄建立失敗：" + ex.ToString());
+            }
+
+            StreamWriter sw = new StreamWriter(key_dir + @"\pub.xml");
+            sw.WriteLine(pub_xml);
+            sw.Close();
+
+            sw = new StreamWriter(key_dir + @"\pvt.xml");
+            sw.WriteLine(pvt_xml);
+            sw.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            using (conn = new SQLiteConnection(connection_str))
+            {
+                conn.Open();
+                sql = @"Insert into Users(Id, Name, Password, SecreKey)
+                    Values(@Id, @Name, @Password, @SecreKey);
+                    ";
+                cmd = new SQLiteCommand(sql, conn);
+                cmd.Parameters.AddWithValue("@Id", textBox1.Text);
+                cmd.Parameters.AddWithValue("@Name", textBox3.Text);
+                cmd.Parameters.AddWithValue("@Password", EncryptRSA(textBox2.Text, pub_xml));
+                cmd.Parameters.AddWithValue("@SecreKey", Generate_secret());
+                cmd.ExecuteNonQuery();
+            }
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            DataTable dt = new DataTable();
+            using (conn = new SQLiteConnection(connection_str))
+            {
+                sql = @"Select Id, Name, Password, SecreKey From Users Where Id = @Id Order by ID";
+                cmd = new SQLiteCommand(sql, conn);
+                cmd.Parameters.AddWithValue("@Id", textBox1.Text);
+                using (SQLiteDataAdapter adapter = new SQLiteDataAdapter(cmd))
+                {
+                    adapter.Fill(dt);
+                }
+            }
+            if (dt.Rows.Count != 1) {
+                MessageBox.Show("帳號或密碼不正確");
+                return;
+            }
+
+            // TODO: 非正確的比對實作方式
+            string pw_input = textBox2.Text;
+            string pw_db = DecryptRSA(dt.Rows[0]["Password"].ToString(), pvt_xml);
+            if (pw_db != pw_input)
+            {
+                MessageBox.Show("帳號或密碼不正確");
+                return;
+            }
+            
+            // dataGridView1.DataSource = dt;
+            string _base32Secret = dt.Rows[0]["SecreKey"].ToString();
+            string AccountName = dt.Rows[0]["Id"].ToString();
+            txtSecret.Text = _base32Secret;
+
+            string otpAuthUri =
+                $"otpauth://totp/{Uri.EscapeDataString(Issuer)}:{Uri.EscapeDataString(AccountName)}" +
+                $"?secret={_base32Secret}" +
+                $"&issuer={Uri.EscapeDataString(Issuer)}" +
+                $"&algorithm=SHA1" +
+                $"&digits=6" +
+                $"&period=30";
+
+            using (QRCodeGenerator qrGenerator = new QRCodeGenerator())
+            using (QRCodeData qrCodeData = qrGenerator.CreateQrCode(otpAuthUri,
+           QRCodeGenerator.ECCLevel.Q))
+            using (QRCode qrCode = new QRCode(qrCodeData))
+            {
+                Bitmap qrBitmap = qrCode.GetGraphic(8);
+                picQr.Image = qrBitmap;
+            }
+            lblResult.Text = "請使用 Google Authenticator 掃描 QR Code。";
+
+
+        }
+    }
+}
